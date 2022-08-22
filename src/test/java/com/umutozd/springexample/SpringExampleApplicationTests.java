8df16d670c5fb5ef9e265112aadc7f02085cc764
Umut <umut@otsimo.com>
2022-08-23 00:01:48 +0300

    initial commit

---
+++ src/test/java/com/umutozd/springexample/SpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umutozd.springexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
